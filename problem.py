abd093aaa49666ca9d09b99ff92640ca65d41910
Balazs Kegl <balazs.kegl@gmail.com>
2017-07-02 17:41:58 +0200

    comments

--- problem.py
+++ problem.py
@@ -7,8 +7,10 @@ problem_title = 'Titanic survival classification'
 _target_column_name = 'Survived'
 _ignore_column_names = ['PassengerId']
 _prediction_label_names = [0, 1]
+# A type (class) which will be used to create wrapper objects for y_pred
 Predictions = rw.prediction_types.multiclass.make_predictions_type(
     label_names=_prediction_label_names)
+# An object implementing the workflow
 workflow = rw.workflows.FeatureExtractorClassifier()
 
 score_types = [
