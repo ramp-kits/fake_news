1ceeacdb52df09e0e2e243bcdd410fd50ae3216e
Balazs Kegl <balazs.kegl@gmail.com>
2017-10-24 22:53:33 +0200

    pep8 problems.py

--- problem.py
+++ problem.py
@@ -5,9 +5,11 @@ from sklearn.model_selection import StratifiedShuffleSplit
 
 problem_title = 'Fake News Detection'
 _target_column_name = 'label'
-_ignore_column_names = ['ID', 'true_counts', 'false_counts', 'half_true_counts', 
-                        'mostly_true_counts', 'pants_on_fire']
-_prediction_label_names = ['barely-true', 'false', 'half-true', 'mostly-true', 'pants-fire', 'true']
+_ignore_column_names = [
+    'ID', 'true_counts', 'false_counts', 'half_true_counts',
+    'mostly_true_counts', 'pants_on_fire']
+_prediction_label_names = [
+    'barely-true', 'false', 'half-true', 'mostly-true', 'pants-fire', 'true']
 # A type (class) which will be used to create wrapper objects for y_pred
 Predictions = rw.prediction_types.make_multiclass(
     label_names=_prediction_label_names)
@@ -23,17 +25,19 @@ def get_cv(X, y):
     cv = StratifiedShuffleSplit(n_splits=8, test_size=0.2, random_state=57)
     return cv.split(X, y)
 
+
 def _read_data(path, f_name):
 
-    data = pd.read_csv(os.path.join(path, 'data', f_name), sep='\t', 
-                       names=['ID', 'label', 'statement', 
-                       'subject', 'speaker', 'job', 'state', 'party', 'true_counts', 
-                       'false_counts', 'half_true_counts', 'mostly_true_counts', 
-                       'pants_on_fire', 'context'])
-    
+    data = pd.read_csv(
+        os.path.join(path, 'data', f_name), sep='\t',
+        names=[
+            'ID', 'label', 'statement', 'subject', 'speaker', 'job', 'state',
+            'party', 'true_counts', 'false_counts', 'half_true_counts',
+            'mostly_true_counts', 'pants_on_fire', 'context'])
+
     y_array = data['label'].values
     X_df = data.drop([_target_column_name] + _ignore_column_names, axis=1)
-    
+
     return X_df, y_array
 
 
